b39961d2c675d4095ee92f7bfb019ffb14b33cba
Esteban Carrillo <75136217+EstebanCarrilloG@users.noreply.github.com>
2022-04-05 09:18:29 -0500

    Update piano_con_arduino_v2_0.ino

--- piano_con_arduino_v2_0/piano_con_arduino_v2_0.ino
+++ piano_con_arduino_v2_0/piano_con_arduino_v2_0.ino
@@ -1,3 +1,12 @@
+/* 
+
+* Código escrito por: Esteban Carrillo para EDEPTEC. 
+* Página web: https://www.edeptec.com
+* Facebook: @edeptec
+* Youtube: https://youtube.com/c/EDEPTEC
+
+*/
+
 #include "pitches.h"
 
 #define speaker 8
