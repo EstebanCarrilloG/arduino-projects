720ecf91a6554aa7cffeb482636a299dfab3e801
Esteban Carrillo <75136217+EstebanCarrilloG@users.noreply.github.com>
2022-04-05 09:16:15 -0500

    Update on__off_y_variar_velocidad_de_parpadeo_con_pulsadores.ino

--- on__off_y_variar_velocidad_de_parpadeo_con_pulsadores/on__off_y_variar_velocidad_de_parpadeo_con_pulsadores.ino
+++ on__off_y_variar_velocidad_de_parpadeo_con_pulsadores/on__off_y_variar_velocidad_de_parpadeo_con_pulsadores.ino
@@ -1,3 +1,12 @@
+/* 
+
+* Código escrito por: Esteban Carrillo para EDEPTEC. 
+* Página web: https://www.edeptec.com
+* Facebook: @edeptec
+* Youtube: https://youtube.com/c/EDEPTEC
+
+*/
+
 #define pul1 A0
 #define pul2 A1
 #define pul3 A2
