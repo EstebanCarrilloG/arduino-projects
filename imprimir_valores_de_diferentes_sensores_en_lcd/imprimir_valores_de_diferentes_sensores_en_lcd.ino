2fe44cea3847a721666242db8e56551d73eff1d3
Esteban Carrillo <75136217+EstebanCarrilloG@users.noreply.github.com>
2022-04-05 09:08:17 -0500

    Update imprimir_valores_de_diferentes_sensores_en_lcd.ino

--- imprimir_valores_de_diferentes_sensores_en_lcd/imprimir_valores_de_diferentes_sensores_en_lcd.ino
+++ imprimir_valores_de_diferentes_sensores_en_lcd/imprimir_valores_de_diferentes_sensores_en_lcd.ino
@@ -1,3 +1,12 @@
+/* 
+
+* Código escrito por: Esteban Carrillo para EDEPTEC. 
+* Página web: https://www.edeptec.com
+* Facebook: @edeptec
+* Youtube: https://youtube.com/c/EDEPTEC
+
+*/
+
 #include <LiquidCrystal.h>
 #define s1 A0
 #define s2 A1
