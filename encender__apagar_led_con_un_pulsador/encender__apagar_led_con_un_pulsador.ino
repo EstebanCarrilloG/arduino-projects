dc249e116855bfa355129920001915c1a0ff3b94
Esteban Carrillo <75136217+EstebanCarrilloG@users.noreply.github.com>
2022-04-05 09:07:41 -0500

    Update encender__apagar_led_con_un_pulsador.ino

--- encender__apagar_led_con_un_pulsador/encender__apagar_led_con_un_pulsador.ino
+++ encender__apagar_led_con_un_pulsador/encender__apagar_led_con_un_pulsador.ino
@@ -1,3 +1,12 @@
+/* 
+
+* Código escrito por: Esteban Carrillo para EDEPTEC. 
+* Página web: https://www.edeptec.com
+* Facebook: @edeptec
+* Youtube: https://youtube.com/c/EDEPTEC
+
+*/
+
 #define led 13
 #define pulsador 2
 
