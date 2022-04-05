319cac2bf4f51a0e287aa923a4c1d63f4478b8af
Esteban Carrillo <75136217+EstebanCarrilloG@users.noreply.github.com>
2022-04-05 09:22:07 -0500

    Update variador_de_velocidad_motor_con_app.ino

--- variador_de_velocidad_motor_con_app/variador_de_velocidad_motor_con_app.ino
+++ variador_de_velocidad_motor_con_app/variador_de_velocidad_motor_con_app.ino
@@ -1,3 +1,12 @@
+/* 
+
+* Código escrito por: Esteban Carrillo para EDEPTEC. 
+* Página web: https://www.edeptec.com
+* Facebook: @edeptec
+* Youtube: https://youtube.com/c/EDEPTEC
+
+*/
+
 #define pwmA  3
 #define motorinA  4
 #define motorinB  5
