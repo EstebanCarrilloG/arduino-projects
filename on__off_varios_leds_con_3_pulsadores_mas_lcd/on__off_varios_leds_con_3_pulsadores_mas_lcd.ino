1ecfae18d44781329c817e21678bdba167fbf552
Esteban Carrillo <75136217+EstebanCarrilloG@users.noreply.github.com>
2022-04-05 09:14:56 -0500

    Update on__off_varios_leds_con_3_pulsadores_mas_lcd.ino

--- on__off_varios_leds_con_3_pulsadores_mas_lcd/on__off_varios_leds_con_3_pulsadores_mas_lcd.ino
+++ on__off_varios_leds_con_3_pulsadores_mas_lcd/on__off_varios_leds_con_3_pulsadores_mas_lcd.ino
@@ -1,3 +1,12 @@
+/* 
+
+* Código escrito por: Esteban Carrillo para EDEPTEC. 
+* Página web: https://www.edeptec.com
+* Facebook: @edeptec
+* Youtube: https://youtube.com/c/EDEPTEC
+
+*/
+
 #include <LiquidCrystal.h>
 
 const int pula = A0;
