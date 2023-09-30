ebfa5453290fc88403542161ba5873c5af8b2029
Esteban Carrillo <estexav0996@gmail.com>
2023-09-30 08:48:00 -0500

    Nombres de variables modificados

--- control_brillo_de_led_con_potrnciometro/control_brillo_de_led_con_potrnciometro.ino
+++ control_brillo_de_led_con_potrnciometro/control_brillo_de_led_con_potrnciometro.ino
@@ -7,24 +7,20 @@
 
 */
 
-#define led 3 // Pin donde va conectado el led
-#define pot A0 // Pin donde va conectado el potenciometro
- 
+#define pinLed 3             // Pin donde va conectado el led
+#define pinPotenciometro A0  // Pin donde va conectado el potenciometro
+
 void setup() {
- 
- pinMode(led,OUTPUT); // Declaramos entradas y salidas
- pinMode(pot,INPUT);
- 
- 
+  pinMode(led, OUTPUT);  // Declaramos entradas y salidas
+  pinMode(pinPotenciometro, INPUT);
 }
- 
+
 void loop() {
- 
-  int pwm; // Declaramos una variable, en esta variable se almacenan los valores que nos entrega la función map
-  pwm = map(analogRead(pot), 0, 1024, 0, 255);
- 
-  analogWrite(led,pwm); // Encendemos el led con el valor del pwm que nos envía le función map
- 
-  delay(100); // agregamos una pequeña pausa
- 
+
+  int pwm;  // Declaramos una variable, en esta variable se almacenan los valores que nos entrega la función map
+  pwm = map(analogRead(pinPotenciometro), 0, 1024, 0, 255);
+
+  analogWrite(pinLed, pwm);  // Encendemos el led con el valor del pwm que nos envía le función map
+
+  delay(100);  // agregamos una pequeña pausa
 }
