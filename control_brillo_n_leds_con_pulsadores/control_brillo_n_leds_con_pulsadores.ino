4bf3e2fd12183897dce9c3db8aad3a9e61a5092d
EstebanCarrilloG <estexav0996@gmail.com>
2024-11-21 11:08:23 -0500

    Codigo mejorado y documentado

--- control_brillo_n_leds_con_pulsadores/control_brillo_n_leds_con_pulsadores.ino
+++ control_brillo_n_leds_con_pulsadores/control_brillo_n_leds_con_pulsadores.ino
@@ -7,63 +7,52 @@
 
 */
 
-int led[] = {3, 5, 6, 9}; // Pines en donde van conectados los leds
-//--- Pines en donde van conectados los pulsadores ---//
-#define pul0 A0
-#define pul1 A1
-#define pul2 A2
-//----------------------//
-int pwm; //Variable que contendrá el valor del pwm
-int x; // Variable para selección de posición del array led
+/* Definimos los pines en donde van conectados los pulsadores */
+#define pulCambio A0
+#define pulSuma A1
+#define pulResta A2
+
+byte ledPins[] = {3, 5, 6, 9}; // Pines en donde van conectados los leds
+byte numeroDeLeds =  sizeof(ledPins); // Número de leds en el array
+byte pwmLeds[sizeof(ledPins)]; // Array para almacenar el valor del pwm de cada led
+bool estadoPrevio = 0; // Variable para controlar el estado previo del pulsador
+
+int pwm; // Variable para almacenar el valor del pwm
+int x;  // Variable para controlar el led actual
 
 void setup() {
   //--- Declaramos entradas y salidas ---//
-  pinMode(pul0, INPUT);
-  pinMode(pul1, INPUT);
-  pinMode(pul2, INPUT);
-  // En este caso como estamos utilizando un array podemos declarar los pines en donde van conectados los leds como salidas, de la siguiente manera:
-  for (x = 0 ; x >= 4; x++) {
-    pinMode(led[x], OUTPUT);
-    Serial.println(x);
+  pinMode(pulCambio, INPUT);
+  pinMode(pulSuma, INPUT);
+  pinMode(pulResta, INPUT);
+  /* Configuramos los pines de los leds como salidas */
+  for (byte x = 0 ; x < numeroDeLeds; x++) {
+    pinMode(ledPins[x], OUTPUT);
   }
-  x = 0; // Enceramos la variable x porque la vamos a utilizar luego en el void loop
 }
 
 void loop() {
 
-  if (digitalRead(pul0) == HIGH) { // Cada vez que presionemos el pulsador conectado al pin A0 sumamos la variable x, esta variable va a tomar los valores de las posiciones del array led que en este caso es de 4, empezando de la posición 0 a la 3. Así seleccionamos el led al cual controlaremos el brillo.
-    x++;
-    delay(1000); // Agregamos una pequeña pausa para el anti rebote
+  bool estadoPulCambio = digitalRead(pulCambio); // Estado lógico del pulsador
 
-    if (x == 4) {
-      x = 0;
+  if (estadoPulCambio != estadoPrevio) { // Implementación antirrebote
+    if (estadoPulCambio == HIGH) { // Cuando el pulsador de cambio de led se presione
+      x++; // Cambiamos de led
+      if (x == numeroDeLeds) x = 0; // Si llegamos al último led, volvemos al primero
+      pwm = pwmLeds[x]; // Obtenemos el valor del pwm del led actual y lo almacenamos en la variable pwm
     }
-
+    delay(10); // Pausa de 10ms
   }
 
-  if (digitalRead(pul1) == HIGH) { //Si presionamos el pulsador conectado al puerto A1, sumamos la variable pwm, aumentando el brillo del led seleccionado
-    pwm++;
-  }
+  estadoPrevio = estadoPulCambio; // Implementación antirrebote
 
-  if (digitalRead(pul2) == HIGH) { //Si presionamos el pulsador conectado al puerto A2, restamos la variable pwm, disminuyendo el brillo del led seleccionado
-    pwm--;
-  }
+  (digitalRead(pulSuma) == HIGH) && pwm++; // Si presionamos el pulsador de suma, sumamos el valor de la variable pwm
+  (digitalRead(pulResta) == HIGH) && pwm--; // Si presionamos el pulsador de resta, restamos el valor de la variable pwm
+  // Limitamos el valor de la variable pwm
+  if (pwm > 255) pwm = 255;
+  if (pwm < 0) pwm = 0;
 
-  //-- Las siguientes lineas de código son para limitar los valores que puede llegar a tomar la variable pwm---//
+  pwmLeds[x] = pwm; // Almacenamos el valor de la variable pwm en la posicion del array perteneciente al led actual
 
-  if (pwm > 255) {
-    pwm = 255;
-  }
-  if (pwm < 0) {
-    pwm = 0;
-  }
-  // ----------------------- //
-
-  analogWrite(led[x], pwm); // Enviamos por el pin en donde esta conectado el led, los valores de la variable pwm. Ejemplo:
-  /* -- int led[] = {3,5,6,9}; // Pines en donde van conectados los leds
-        posiciones  {0,1,2,3}
-
-    Si x = 1(led[1]) entonces estaremos trabajando con el led conectado en el pin 5.
-    Si x = 3(led[3]) entonces estaremos trabajando con el led conectado en el pin 9.
-  */
+  analogWrite(ledPins[x], pwmLeds[x]); // Enviamos por el pin en donde esta conectado el led, el valor del pwm que le pertenece
 }
