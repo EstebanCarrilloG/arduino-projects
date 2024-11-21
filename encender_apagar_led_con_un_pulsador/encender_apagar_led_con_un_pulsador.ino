fae640e3443612dda3d850b9f6c6d4eb103dcba0
EstebanCarrilloG <estexav0996@gmail.com>
2024-11-21 08:33:04 -0500

    encender_apagar_led_con_un_pulsador updated

---
+++ encender_apagar_led_con_un_pulsador/encender_apagar_led_con_un_pulsador.ino
@@ -0,0 +1,35 @@
+/* 
+
+* Código escrito por: Esteban Carrillo para EDEPTEC. 
+* Página web: https://www.edeptec.com
+* Facebook: @edeptec
+* Youtube: https://youtube.com/c/EDEPTEC
+
+*/
+
+#define led 13 // led en el pin 13
+#define pulsador 2 // pulsador en el pin 2
+
+bool estadoDelLed = 0; // Variable para controlar el estado del led
+bool estadoPrevio = 0; // Variable para controlar el estado previo del pulsador
+
+void setup()
+{
+  pinMode(led, OUTPUT); // led como salida
+  pinMode(pulsador, INPUT);// pulsador como entrada
+  digitalWrite(led, 0); // led apagado por defecto
+}
+
+void loop()
+{
+  bool estadoPulsador = digitalRead(pulsador); // Estado lógico del pulsador
+  if (estadoPulsador  != estadoPrevio) { // Implementación antirrebote
+    if (estadoPulsador == HIGH) estadoDelLed = !estadoDelLed; // Cuando el pulsador se presione, cambia el valor de la variable estadoDelLed
+  }
+
+  delay(10); // Pausa de 10ms
+
+  estadoPrevio = estadoPulsador; // Implementación antirrebote
+  
+  digitalWrite(led, estadoDelLed); // Se enciende o apaga el led dependiendo de el valor de la variable estadoDelLed
+}
