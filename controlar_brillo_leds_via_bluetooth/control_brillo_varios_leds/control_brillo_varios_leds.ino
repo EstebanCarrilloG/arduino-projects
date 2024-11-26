f653d8252c5eeec89ce663c8fa73d0b781883184
EstebanCarrilloG <estexav0996@gmail.com>
2024-11-26 17:13:55 -0500

    Codigo refactorizado: - Libreria ArduinoJson.h añadida

--- controlar_brillo_leds_via_bluetooth/control_brillo_varios_leds/control_brillo_varios_leds.ino
+++ controlar_brillo_leds_via_bluetooth/control_brillo_varios_leds/control_brillo_varios_leds.ino
@@ -6,41 +6,63 @@
 * Youtube: https://youtube.com/c/EDEPTEC
 
 */
+#include <ArduinoJson.h>  // Libreria para convertir string a JSON
 
-int leds[3] = {5, 6, 9};
+int leds[3] = { 5, 6, 9 };  // Pines en donde van conectados los leds/
 
 String str = "";
-const char separador = ',';
-const int numeroDeDatos = 3;
-int dato[numeroDeDatos];
+const int numeroDeDatos = 3;  // Numero de datos a recibir
+int datos[numeroDeDatos];     // Array para almacenar los valores de los datos recibidos
 
-void setup()
-{
+/**
+ * Configura los pines de los LEDs como salidas y establece la velocidad de
+ * comunicación serial.
+ */
+void setup() {
   Serial.begin(9600);
-  for (int i = 0; i < 4; i++) {
+  for (int i = 0; i < numeroDeDatos; i++) {
     pinMode(leds[i], OUTPUT);
   }
 }
 
-void loop()
-{
-  if (Serial.available())
-  {
-    str = Serial.readStringUntil(']');
-    str.remove(0, 1);
-   
+/**
+ * Bucle principal del programa.
+ *
+ * En este bucle se verifica si hay datos disponibles en el puerto serie,
+ * si es así se almacenan en un string y se convierten a un objeto JSON.
+ * Luego se extraen los valores de los datos recibidos y se almacenan en un
+ * array. Finalmente se actualiza el brillo de los LEDs con los valores
+ * del array.
+ */
+void loop() {
+  JsonDocument doc;          // Asigna el documento JSON a doc
+  if (Serial.available()) {  // Verificar si hay datos disponibles
 
 
-    for (int i = 0; i < numeroDeDatos ; i++)
-    {
-      int index = str.indexOf(separador);
-      dato[i] = str.substring(0, index).toInt();
-      str = str.substring(index + 1);
+    // Leer hasta el final del string
+    str = Serial.readStringUntil('}');  //String en este punto {"value1":255,"value2":255,"value3":255
+
+    // Agregar "}" al final del string
+    str.concat("}");  //String en este punto: {"value1":255,"value2":255,"value3":255}
+
+    Serial.println(str);  // Imprime el string en el monitor serial
+
+    DeserializationError error = deserializeJson(doc, str);  // Convertir el string a JSON
+
+    if (error) {  // Verificar si hubo un error en la conversion
+      Serial.print(F("deserializeJson() failed: "));
+      Serial.println(error.f_str());
+      return;
     }
 
+    // Extrae los valores de los datos recibidos y los almacena en el array dato
+    datos[0] = doc["value1"].as<int>();
+    datos[1] = doc["value2"].as<int>();
+    datos[2] = doc["value3"].as<int>();
   }
-   
+
+  // Actualizar el brillo de los LEDs
   for (int i = 0; i < numeroDeDatos; i++) {
-    analogWrite(leds[i], dato[i]);
+    analogWrite(leds[i], datos[i]);
   }
 }
